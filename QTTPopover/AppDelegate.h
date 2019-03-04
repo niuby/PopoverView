73516fc8d36d1094029f2ba8b9fafe52545a96e0
武恩泽 <wuenze@qutoutiao.net>
2019-03-04 17:28:03 +0800

    Initial Commit

---
+++ QTTPopover/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QTTPopover
+//
+//  Created by 武恩泽 on 2019/3/4.
+//  Copyright © 2019 武恩泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QTTPopover/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QTTPopover
+//
+//  Created by 武恩泽 on 2019/3/4.
+//  Copyright © 2019 武恩泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
